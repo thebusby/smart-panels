c90d4f1eb927687a56f227b0846ed4b768174225
Alan Busby <abusby@suginamio.com>
2023-05-16 09:55:17 +0900

    Checkpoint, compiles

--- TestPanel/TestPanel/Panel.h
+++ TestPanel/TestPanel/Panel.h
@@ -3,10 +3,7 @@
 #include "Arduino.h" 
 #include "PCF8575.h"
 
-
-// Usage notes;
-// 1. Each IOMethod used should have IOMethod.setup() called during setup()
-// 2. tc_updatec() should be called at start of each loop()
+#define SERIAL_BUFFER_SIZE 64 // 64 - "\r\n"
 
 
 
@@ -141,8 +138,10 @@ class MyPCF8575 {
         }
 
         // Panel calls this after all compononents
-        bool setup(uint16_t mask) {
+        bool setup() {
             _module->write16(_dataOut);
+
+            return true;
         }
 
         bool read(uint8_t pin) {
@@ -380,6 +379,8 @@ class ButtonComponent: public InputComponent {
         bool _state;
 };
 
+
+
 //
 // Define a panel
 //
@@ -410,7 +411,141 @@ class Panel: public Component {
 
             return true;
         }
+
+        bool loop();
 };
 
+
+
+/*
+ * Handle general panel logic
+*/
+typedef struct cmd {
+  char *cmd_name;
+  char* (*cmd_func)(Panel*, char*);
+} cmd_t;
+
+/* Protocol handlers */
+char* com_prot_ident   (Panel*, char*);
+char* com_prot_desc    (Panel*, char*);
+char* com_prot_ping    (Panel*, char*);
+char* com_prot_set     (Panel*, char*);
+char* com_prot_get     (Panel*, char*);
+
+/* List of commands */
+cmd_t command[] = {
+  { "IDENT",    com_prot_ident },
+  { "PING",     com_prot_ping },
+  { "DESC",     com_prot_desc },
+  { "SET",      com_prot_set },
+  { "GET",      com_prot_get },
+  { 0 }
+};
+
+/* Tokenize space delimited text */
+char* pop_token(char* input, char** next) {
+    int i;
+    char* token = input;
+
+    for(i=0; input[i]; i++) {
+        if( (input[i] == ' ')
+            || (input[i] == '\t')
+            || (input[i] == '\r')
+            ) { // Handle all whitespace, \r ESPECIALLY!
+            input[i] = '\0';
+
+            i++; // Increment into next field
+            // if(input[i] != '\n') // This is handled by NULL termination of string
+                *next = &(input[i]);
+            // else
+            //    *next = NULL; // Return NULL if we hit \r\n
+
+            return token;
+        }
+    }
+
+    // Hit last field
+    if(i>0) {
+        *next = NULL;
+        return token;
+    }
+
+    return NULL;
+}
+
+
+
+
+bool Panel::loop() {
+    char buf[SERIAL_BUFFER_SIZE];
+    InputComponent *input = NULL;
+
+    // Increment Tick counter
+    tc_update();
+
+    // Check Serial
+    if(Serial.available() > 0) {
+        int i;
+        char* cmd;
+        char* args;
+        String str = Serial.readStringUntil('\n');
+
+        str.toCharArray(buf, (SERIAL_BUFFER_SIZE - 1));
+        buf[(SERIAL_BUFFER_SIZE - 1)] = '\0'; // NULL terminate, just in case
+
+        cmd = pop_token(buf, &args);
+        if(cmd) {
+            for(i = 0; command[i].cmd_name != 0; i++) {
+                if(strcasecmp(cmd, command[i].cmd_name) == 0)
+                    break;
+            }
+        }
+
+        if(command[i].cmd_name == 0) {
+            Serial.println("ERR Command not found");
+            Serial.flush();
+        } else {
+            char* output = (command[i].cmd_func)(this, args);
+
+            if(output) {
+                Serial.println(output);
+                Serial.flush();
+            }
+        }
+    }
+
+    // Check Inputs
+    for(input=inputs[0]; input; input++) {
+        if(input->poll()) {
+            input->getMessage(buf);
+            Serial.println(buf);
+            Serial.flush();
+        }
+    }
+
+
+    delay(10);
+}
+
+
+char* com_prot_ident   (Panel* panel, char* args) {
+    return panel->id;
+}
+
+char* com_prot_ping    (Panel* panel, char* args) {
+    return "PONG";
+}
+
+char* com_prot_desc    (Panel* panel, char* args) {
+    return "DESC";
+}
+
+char* com_prot_set     (Panel* panel, char* args) {
+    return "TODO";
+}
+
+char* com_prot_get     (Panel* panel, char* args) {
+    return "ACK";
+}
 
 #endif
--- TestPanel/TestPanel/TestPanel.ino
+++ TestPanel/TestPanel/TestPanel.ino
@@ -1,6 +1,5 @@
 #include "Panel.h"
 
-#define SERIAL_BUFFER_SIZE 62 // 64 - "\r\n"
 
 
 /*
@@ -25,126 +24,21 @@ OutputComponent *outputs[] =
 Panel *panel = new Panel("TEST_PANEL", inputs, outputs);
 
 
-/*
- * Handle general panel logic
-*/
-typedef struct cmd {
-  char *cmd_name;
-  char* (*cmd_func)(char*);
-} cmd_t;
-
-/* Protocol handlers */
-char* com_prot_ident   (char*);
-char* com_prot_ping    (char*);
-char* com_prot_set     (char*);
-char* com_prot_get     (char*);
-
-/* List of commands */
-cmd_t command[] = {
-  { "IDENT",    com_prot_ident },
-  { "PING",     com_prot_ping },
-  { "SET",      com_prot_set },
-  { "GET",      com_prot_get },
-  { 0 }
-};
-
-/* Tokenize space delimited text */
-char* pop_token(char* input, char** next) {
-    int i;
-    char* token = input;
-
-    for(i=0; input[i]; i++) {
-        if( (input[i] == ' ')
-            || (input[i] == '\t')
-            || (input[i] == '\r')
-            ) { // Handle all whitespace, \r ESPECIALLY!
-            input[i] = '\0';
-
-            i++; // Increment into next field
-            if(input[i] != '\n')
-                *next = &(input[i]);
-            else
-                *next = NULL; // Return NULL if we hit \r\n
-
-            return token;
-        }
-    }
-
-    // Hit last field
-    if(i>0) {
-        *next = NULL;
-        return token;
-    }
-
-    return NULL;
-}
-
-
 
 void setup() {
     Serial.begin(115200);
+
+    // Initialize panel
+    if(!panel->setup()) {
+        // Panel init failed, do something!
+    }
+
+    // Initialize IO expander
+    if(!PCF->setup()) {
+        // IO Expander init failed, do something!
+    }
 }
 
 void loop() {
-    char buf[SERIAL_BUFFER_SIZE];
-    InputComponent *input = NULL;
-
-    // Check Serial
-    if(Serial.available() > 0) {
-        int i;
-        char* cmd;
-        char* args;
-        String str = Serial.readStringUntil('\n');
-
-        str.toCharArray(buf, (SERIAL_BUFFER_SIZE - 1));
-        buf[SERIAL_BUFFER_SIZE] = '\0'; // NULL terminate, just in case
-
-        cmd = pop_token(buf, &args);
-        if(cmd) {
-            for(i = 0; command[i].cmd_name != 0; i++) {
-                if(strcasecmp(cmd, command[i].cmd_name) == 0)
-                    break;
-            }
-        }
-
-        if(command[i].cmd_name == 0) {
-            Serial.println("ERR Command not found");
-            Serial.flush();
-        } else {
-            char* output = (command[i].cmd_func)(args);
-
-            if(output) {
-                Serial.println(output);
-                Serial.flush();
-            }
-        }
-    }
-
-    // Check Inputs
-    for(input=inputs[0]; input; input++) {
-        if(input->poll()) {
-            input->getMessage(buf);
-            Serial.println(buf);
-            Serial.flush();
-        }
-    }
-
-
-    delay(10);
-}
-
-char* com_prot_ident   (char* args) {
-    return panel->id;
-}
-
-char* com_prot_ping    (char* args) {
-    return "PONG";
-}
-
-char* com_prot_set     (char* args) {
-    return "TODO";
-}
-
-char* com_prot_get     (char* args) {
-    return "ACK";
+    panel->loop();
 }
